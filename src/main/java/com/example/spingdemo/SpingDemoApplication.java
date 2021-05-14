78fb0bfa46a02ecac6c95bd4c2331cead20ae469
kateburtseva <burcevakate@gmail.com>
2021-05-14 15:31:55 +0300

    Initial commit

---
+++ src/main/java/com/example/spingdemo/SpingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingDemoApplication.class, args);
+	}
+
+}
