fa08fa6a2ad8d605b5ba82582dd7fb3b7933ff2e
kateburtseva <burcevakate@gmail.com>
2021-05-16 18:04:53 +0300

    data access in spring

--- src/main/java/com/example/spingdemo/SpingDemoApplication.java
+++ src/main/java/com/example/spingdemo/SpingDemoApplication.java
@@ -1,13 +1,53 @@
 package com.example.spingdemo;
 
+import com.example.spingdemo.data.entity.Room;
+import com.example.spingdemo.data.repository.GuestRepository;
+import com.example.spingdemo.data.repository.ReservationRepository;
+import com.example.spingdemo.data.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpingDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpingDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpingDemoApplication.class, args);
+    }
+
+    @RestController
+    @RequestMapping("/rooms")
+    public class RoomController {
+        @Autowired
+        private RoomRepository roomRepository;
+
+        @GetMapping
+        private Iterable<Room> getRooms() {
+            return this.roomRepository.findAll();
+        }
+    }
+
+    @RestController
+    @RequestMapping("/guests")
+    public class GuestController{
+        @Autowired
+        private GuestRepository guestRepository;
+
+        @GetMapping
+        public Iterable<com.frankmoley.lil.learningspring.data.entity.Guest> getGuests(){return this.guestRepository.findAll();}
+    }
+
+    @RestController
+    @RequestMapping("/reservations")
+    public class ReservationController{
+        @Autowired
+        private ReservationRepository reservationRepository;
+
+        @GetMapping
+        public Iterable<com.frankmoley.lil.learningspring.data.entity.Reservation> getReservations(){return this.reservationRepository.findAll();}
+    }
 
 }
---
+++ src/main/java/com/example/spingdemo/data/repository/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.example.spingdemo.data.repository;
+
+import com.frankmoley.lil.learningspring.data.entity.Reservation;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+}
