4756fb0ba7deb41720ca167349d4d77a7833d573
kateburtseva <burcevakate@gmail.com>
2021-05-17 21:26:24 +0300

    web page with spring part #1

---
+++ src/main/java/com/example/spingdemo/web/RoomReservationWebController.java
@@ -0,0 +1,17 @@
+package com.example.spingdemo.web;
+
+import com.example.spingdemo.business.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/reservations")
+public class RoomReservationWebController {
+    private final ReservationService reservationService;
+
+    @Autowired
+    public RoomReservationWebController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+}
